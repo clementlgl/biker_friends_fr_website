83c99ba56400aeb937aff0ee48327e6e1cc432bd
Clément Laigle <c.laigle@catie.fr>
2026-05-08 14:37:49 +0000

    fix: update base path for production environment in Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/biker_friends_fr_website/' : '/',
   integrations: [mdx()],
   vite: {
     plugins: [tailwindcss()]
